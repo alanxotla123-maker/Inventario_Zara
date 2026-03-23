ee5d4f346a4fc2975cfabe5bdf76e0091f301c6f
alanxotla123 <alanxotla123@gmail.com>
2026-03-23 11:40:59 -0600

    STYLE: se agrego la pagina inventario solo es de vista de momento

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,9 @@
-console.log("mensaje de prueba")
+ function mostrarInventario() {
+            document.getElementById("dashboard-section").style.display = "none";
+            document.getElementById("inventory-section").style.display = "block";
+        }
+
+        function mostrarDashboard() {
+            document.getElementById("dashboard-section").style.display = "block";
+            document.getElementById("inventory-section").style.display = "none";
+        }
